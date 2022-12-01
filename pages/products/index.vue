22d9b1d4717bedf69eaf54641b66ce078c25e157
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2022-12-01 19:08:26 +0100

    only push

---
+++ pages/products/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="container">
+      <LargeCardDisplay :cardsSection="largeCardInfo[0]" />
+
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { smallCardSections, largeCardSections } from "../../assets/data";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
